50123630d624e6871ad6cf5e5692bd8fac1004be
Will Robertson <williamdrobertson42@gmail.com>
2023-08-19 16:37:04 +1000

    feat: add outline theme

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,20 @@ import classNames from "classnames";
 
 const ButtonColor = {
   primary:
-    "border-primary bg-slate-100 focus:ring ring-primary/50 transition-color hover:bg-slate-200 dark:text-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700",
+    "border-primary bg-slate-100 ring-primary/50 transition-color hover:bg-slate-200 dark:text-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700",
+  outline:
+    "border-black bg-slate-100 ring-black/50 transition-color hover:bg-slate-200 dark:border-white dark:text-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700",
 };
 
 const everyButton =
-  "border-4 rounded-2xl font-sans font-semibold shadow-md p-2 transition-transform active:scale-95";
+  "border-4 rounded-2xl font-sans font-semibold shadow-md p-2 focus:ring transition-transform active:scale-95";
 
-const Button = ({ buttonText }) => {
-  const finalStyles = classNames([everyButton, ButtonColor["primary"]]);
+const Button = ({ buttonText, buttonTheme }) => {
+  if (!buttonTheme) {
+    buttonTheme = "primary";
+  }
+
+  const finalStyles = classNames([everyButton, ButtonColor[buttonTheme]]);
 
   return <button className={finalStyles}>{buttonText}</button>;
 };
