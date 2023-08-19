17d1b2d5725e41407310f572d03d321ba910e7eb
Will Robertson <williamdrobertson42@gmail.com>
2023-08-19 17:22:26 +1000

    feat: accept further classNames

--- src/components/StyledLink.tsx
+++ src/components/StyledLink.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 type StyledLinkProps = {
   linkText: string;
   href: string;
-  replace: boolean;
-  scroll: boolean;
-  prefetch: boolean;
+  replace?: boolean;
+  scroll?: boolean;
+  prefetch?: boolean;
+  className?: string;
 };
 
-const StyledLink = ({ linkText, href, replace, scroll, prefetch }: StyledLinkProps) => {
+const StyledLink = ({ linkText, href, replace, scroll, prefetch, className }: StyledLinkProps) => {
   return (
     <Link
-      className="border-4 rounded-2xl font-sans font-semibold shadow-md p-2 focus:ring transition-transform active:scale-95 border-black bg-slate-100 ring-black/50 transition-color hover:bg-slate-200 dark:border-white dark:text-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700"
+      className={
+        "border-2 rounded-2xl font-sans font-semibold shadow-md p-2 focus:ring transition-transform active:scale-95 border-slate-400 bg-slate-100 ring-black/50 transition-color hover:bg-slate-200 dark:border-white dark:text-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700 " +
+        className
+      }
       href={href}
       replace={replace}
       scroll={scroll}
