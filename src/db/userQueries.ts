a4b1ada03138e8e47857d685dcc24d25716c5ee0
Will Robertson <williamdrobertson42@gmail.com>
2023-08-22 20:34:35 +1000

    feat: add types and more idiomatic db usage

--- src/db/userQueries.ts
+++ src/db/userQueries.ts
@@ -1,48 +1,65 @@
+import { Patient, User, UserToInsert } from "@/types";
 import postgres from "postgres";
 
-const createDoctorUser = async (db: postgres.Sql, doctor: User) => {
-
+const createUser = async (db: postgres.Sql, doctor: UserToInsert) => {
     if (!doctor.pass) {
       throw new Error("No password provided");
     }
 
-    const [dbUser] = await db`
+  const [dbUser]: [User?] = await db`
       INSERT INTO
       users ${db(doctor)}
-      RETURNING user_id, first_name, last_name, email, email_verified, ahpra, role
+      RETURNING id, first_name, last_name, email, email_verified, ahpra, role
   `;
+
+  if (!dbUser) {
+    throw new Error("Something went wrong in creation");
+  }
 
     return dbUser;
 };
 
 const createPatient = async (db: postgres.Sql, patient: Patient) => {
-
-    const [dbPatient] = await db`
+  const [dbPatient]: [Patient?] = await db`
       INSERT INTO
       patients ${db(patient)}
       RETURNING patient_id, first_name, last_name, email, dob
   `;
+
+  if (!dbPatient) {
+    throw new Error("Something went wrong in creation");
+  }
     return dbPatient;
 };
 
-const getUserCols = ["userId", "role", "firstName", "lastName", "email", "emailVerified"]
+const getUserCols = ["id", "role", "firstName", "lastName", "email", "emailVerified"];
 
 const getUserById = async (db: postgres.Sql, userId: string) => {
-
-  const [foundUser] = await db`
+  const [foundUser]: [User?] = await db`
   SELECT ${db(getUserCols)}
   FROM users
-  WHERE user_id = ${userId}`;
+  WHERE id = ${userId}`;
+
+  if (!foundUser) {
+    throw new Error("User not found");
+  }
 
   return foundUser;
 };
 
 const getUserByEmail = async (db: postgres.Sql, email: string) => {
-
-  const [foundUser] = await db`
-  SELECT user_id, role, first_name, last_name, email, email_verified
+  const [foundUser]: [User?] = await db`
+  SELECT ${db(getUserCols)}
   FROM users
   WHERE email = ${email}`;
+
+  if (!foundUser) {
+    throw new Error("User not found");
+  }
+
+  return foundUser;
+};
+
 
   return foundUser;
 };
