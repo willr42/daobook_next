603617fc199562305beaddd97aa53d4106532c11
Will Robertson <williamdrobertson42@gmail.com>
2023-08-26 12:13:19 +1000

    feat: add signout function to handler

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { signOut } from "next-auth/react";
 import Button from "./Button";
 
 const LogoutButton = () => {
-  return <Button buttonText="Logout" onClick={(e) => console.log(e)} />;
+  return <Button buttonText="Logout" onClick={() => signOut()} />;
 };
 
 export default LogoutButton;
