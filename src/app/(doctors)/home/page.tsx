51127c0266d555dea54bd1520e2023ad63ad0294
Will Robertson <williamdrobertson42@gmail.com>
2023-08-26 12:23:45 +1000

    feat: dashboard buttons

--- src/app/(doctors)/home/page.tsx
+++ src/app/(doctors)/home/page.tsx
@@ -1,5 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import StyledLink from "@/components/StyledLink";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
@@ -12,8 +14,19 @@ export default async function Dashboard() {
 
   return (
     <>
-      <h1>Protected page</h1>
-      <p>Signed in as {userEmail}</p>
+      <h1>Welcome to your dashboard.</h1>
+      <div className="flex h-48 gap-5">
+        <StyledLink
+          href="./new-consult"
+          linkText="New Patient"
+          className="flex flex-col justify-center p-5"
+        />
+        <StyledLink
+          href="./patient-list"
+          linkText="Patient List"
+          className="flex flex-col justify-center p-5"
+        />
+      </div>
     </>
   );
 }
--- src/app/(doctors)/layout.tsx
+++ src/app/(doctors)/layout.tsx
@@ -1,15 +1,10 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
 import DoctorHeader from "@/components/DoctorHeader";
 
 export default function DoctorLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <DoctorHeader />
-      <SessionProvider>
-        <div className="flex flex-col items-center gap-7">{children}</div>
-      </SessionProvider>
+      <div className="mt-5 flex flex-col items-center gap-7">{children}</div>
     </>
   );
 }
