8758878809281f56244fa4c61dcfecf8557bd23a
Will Robertson <williamdrobertson42@gmail.com>
2023-08-29 18:03:46 +1000

    feat: build out form inputs

--- src/app/(doctors)/patients/new/NewPatientForm.tsx
+++ src/app/(doctors)/patients/new/NewPatientForm.tsx
@@ -2,8 +2,10 @@
 
 import { startTransition } from "react";
 import Input from "@/components/Input";
+import Button from "@/components/Button";
 import { useForm } from "react-hook-form";
 import { action, type FormData } from "./newPatientAction";
+import DateInput from "@/components/DateInput";
 
 export default function NewPatientForm() {
   const {
@@ -19,8 +21,36 @@ export default function NewPatientForm() {
   });
 
   return (
-    <form onSubmit={onSubmit}>
-      <Input label="First Name" register={register} defaultValue="Sue" required />
+    <form onSubmit={onSubmit} className="flex flex-col gap-6">
+      <Input
+        label="First Name"
+        fieldName="firstName"
+        register={register}
+        defaultValue="Sue"
+        required={true}
+      />
+      <Input
+        label="Last Name"
+        fieldName="lastName"
+        register={register}
+        defaultValue="Richards"
+        required={true}
+      />
+      <Input
+        label="Email"
+        fieldName="email"
+        register={register}
+        defaultValue="sue@example.com"
+        required={true}
+      />
+      <DateInput
+        label="Date of Birth"
+        fieldName="dob"
+        register={register}
+        defaultValue={new Date(1985, 9, 26)}
+        required={true}
+      />
+      <Button buttonType="submit" buttonText="Add Patient" />
     </form>
   );
 }
