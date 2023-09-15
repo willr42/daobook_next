ddf420e9f11a760e331d9da03acd6b6353ed7fb3
Will Robertson <williamdrobertson42@gmail.com>
2023-09-15 12:51:33 +1000

    fix: correct typing

--- src/db/consultQueries.ts
+++ src/db/consultQueries.ts
@@ -29,7 +29,7 @@ const getConsults = async (db: postgres.Sql, patientId: string) => {
   return foundConsults;
 };
 
-const getConsult = async (db: postgres.Sql, patientId: string, consultId: number) => {
+const getConsult = async (db: postgres.Sql, patientId: string, consultId: string) => {
   if (!consultId) {
     return null;
   }
