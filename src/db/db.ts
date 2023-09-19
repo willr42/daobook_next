d8a7250babae95449b63bff67a03126e51c0abf0
Will Robertson <williamdrobertson42@gmail.com>
2023-09-19 18:48:54 +1000

    fix: update config with envs

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,17 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
-console.log("envs: ", dotenv);
 import postgres from "postgres";
 
-const sql = postgres({ transform: postgres.camel });
+const portNumber = parseInt(process.env.POSTGRES_PORT as string);
+
+const options: postgres.Options<{}> = {
+  transform: postgres.camel,
+  host: process.env.POSTGRES_HOST,
+  port: portNumber,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_USER,
+  pass: process.env.POSTGRES_PASSWORD,
+};
+console.log("sql options", options);
+
+const sql = postgres(process.env.POSTGRES_URL as string, options);
 
 export default sql;
