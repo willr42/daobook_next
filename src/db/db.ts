778db9e558c701f11e43623078b6db436acc0099
Will Robertson <williamdrobertson42@gmail.com>
2023-09-19 18:56:38 +1000

    fix: try other ssl setting

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,7 +5,7 @@ const portNumber = parseInt(process.env.POSTGRES_PORT as string);
 const options: postgres.Options<{}> = {
   transform: postgres.camel,
   host: process.env.POSTGRES_HOST,
-  ssl: false,
+  ssl: "require",
   port: portNumber,
   database: process.env.POSTGRES_DATABASE,
   user: process.env.POSTGRES_USER,
