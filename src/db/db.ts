aa234ed3dcf711d3ebf1f5b034f527ec73a078ee
Will Robertson <48553811+willr42@users.noreply.github.com>
2023-09-19 18:39:14 +1000

    temp: debugging (#3)

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
+console.log("envs: ", dotenv);
 import postgres from "postgres";
 
-const sql = postgres({transform: postgres.camel});
+const sql = postgres({ transform: postgres.camel });
 
 export default sql;
